cefb608401152bdd90deb2c97490b5a4a7c4dc82
AkshayThakur1995 <akshaymarx08@gmail.com>
2022-04-05 11:02:13 +0530

    https req

--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,70 +1,79 @@
-import { createSlice, createAsyncThunk} from "@reduxjs/toolkit"
-import axios from "axios"
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-export const fetchMovies = createAsyncThunk("movies/fetchMovies", async(term) => {
-    
+export const fetchMovies = createAsyncThunk(
+  "movies/fetchMovies",
+  async (term) => {
     const response = await axios.get(
-        `http://www.omdbapi.com/?i=tt3896198&apikey=97084054&s=${term}`);
-        return response.data
-})
+      `https://www.omdbapi.com/?i=tt3896198&apikey=97084054&s=${term}`
+    );
+    return response.data;
+  }
+);
 
-export const fetchMoviesShows = createAsyncThunk("movies/fetchMoviesShows", async(term) => {
-
+export const fetchMoviesShows = createAsyncThunk(
+  "movies/fetchMoviesShows",
+  async (term) => {
     const response = await axios.get(
-        `http://www.omdbapi.com/?i=tt3896198&apikey=97084054&s=${term}&type=series`);
-        return response.data
-})
+      `https://www.omdbapi.com/?i=tt3896198&apikey=97084054&s=${term}&type=series`
+    );
+    return response.data;
+  }
+);
 
-export const fetchMoviesOrShowsDetail = createAsyncThunk("movies/fetchMoviesOrShowsDetail", async(id) => {
+export const fetchMoviesOrShowsDetail = createAsyncThunk(
+  "movies/fetchMoviesOrShowsDetail",
+  async (id) => {
     const response = await axios.get(
-        `http://www.omdbapi.com/?&apikey=97084054&i=${id}&plot=full`);
-        return response.data
-})
-
-
+      `https://www.omdbapi.com/?&apikey=97084054&i=${id}&plot=full`
+    );
+    return response.data;
+  }
+);
 
 const initialState = {
-    movies:{},
-    shows:{},
-    selectedMovieOrShow:{}
-}
+  movies: {},
+  shows: {},
+  selectedMovieOrShow: {},
+};
 
-const movieSlice =  createSlice({
-    name:"movies",
-    initialState,
-    reducers:{
-        addMovies:(state, {payload}) => {
-            state.movies = payload;
-        },
-        removeSelected:(state) => {
-            state.selectedMovieOrShow = {}
-        }
+const movieSlice = createSlice({
+  name: "movies",
+  initialState,
+  reducers: {
+    addMovies: (state, { payload }) => {
+      state.movies = payload;
     },
-    extraReducers: {
-        [fetchMovies.pending]: () => {
-            console.log("Pending")
-        },
-        [fetchMovies.fulfilled]: (state, {payload}) => {
-            console.log("Fetched sucess")
-            return {...state, movies: payload}
-        },
-        [fetchMovies.rejected]: () => {
-            console.log("Call Rejected")
-        },
-        [fetchMoviesShows.fulfilled]: (state,{payload}) => {
-            console.log("Fetched sucessful");
-            return{...state, shows: payload}
-        },
-        [fetchMoviesOrShowsDetail.fulfilled]: (state, {payload}) => {
-            console.log("Fetched sucessful");
-            return{...state, selectedMovieOrShow: payload}
-        }
-    }
-})
+    removeSelected: (state) => {
+      state.selectedMovieOrShow = {};
+    },
+  },
+  extraReducers: {
+    [fetchMovies.pending]: () => {
+      console.log("Pending");
+    },
+    [fetchMovies.fulfilled]: (state, { payload }) => {
+      console.log("Fetched sucess");
+      return { ...state, movies: payload };
+    },
+    [fetchMovies.rejected]: () => {
+      console.log("Call Rejected");
+    },
+    [fetchMoviesShows.fulfilled]: (state, { payload }) => {
+      console.log("Fetched sucessful");
+      return { ...state, shows: payload };
+    },
+    [fetchMoviesOrShowsDetail.fulfilled]: (state, { payload }) => {
+      console.log("Fetched sucessful");
+      return { ...state, selectedMovieOrShow: payload };
+    },
+  },
+});
 
-export const {addMovies} = movieSlice.actions
-export const {removeSelected} = movieSlice.actions
-export const getAllMovies = (state) => state.movies.movies
-export const getAllShows = (state) => state.movies.shows
-export const getSelectedMovieOrShow = (state) => state.movies.selectedMovieOrShow
-export default movieSlice.reducer
+export const { addMovies } = movieSlice.actions;
+export const { removeSelected } = movieSlice.actions;
+export const getAllMovies = (state) => state.movies.movies;
+export const getAllShows = (state) => state.movies.shows;
+export const getSelectedMovieOrShow = (state) =>
+  state.movies.selectedMovieOrShow;
+export default movieSlice.reducer;
